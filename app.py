b1160fca8c8bc112ba2643fbbf232e938a191092
Manigandan <manigandans.tn@gmail.com>
2025-05-04 20:27:23 +0530

    Fix app.run() call with host and port

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask
+from flask_cors import CORS  
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  
 
 @app.route("/")
 def home():
     return "Tambola backend is running!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
